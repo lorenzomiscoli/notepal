790d7b36d9faf59fb0399038e4022966ffb2751e
Lorenzo <lmiscoli@gmail.com>
2023-12-17 09:35:39 +0100

    make add button bigger

--- src/app/layouts/notes/components/notes-list-footer/notes-list-footer.component.ts
+++ src/app/layouts/notes/components/notes-list-footer/notes-list-footer.component.ts
@@ -1,14 +1,14 @@
-import { Component, Input } from "@angular/core";
+import { Component } from "@angular/core";
 
 import { NOTES_LIST_FOOTER_DEPS } from "./notes-list-footer.dependencies";
 
 @Component({
   selector: "app-notes-list-footer",
   templateUrl: "./notes-list-footer.component.html",
+  styleUrls: ["./notes-list-footer.component.scss"],
   standalone: true,
   imports: [NOTES_LIST_FOOTER_DEPS]
 })
 export class NotesListFooterComponent {
-  @Input() public selectedMode!: boolean;
 
 }
